22d6e933caf77a10b2e50dfaf66045f6ddc41023
Eric Glover <etgt28@mail.missouri.edu>
2017-08-21 13:01:07 -0500

    things??

--- food/src/App.js
+++ food/src/App.js
@@ -3,6 +3,27 @@ import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./nav.js";
 
+const menu = {
+  drinks: [
+    {
+      name: "Whisky",
+      price: "$100"
+    }
+  ],
+  food: [
+    {
+      name: "WhiskyBread",
+      price: "$100"
+    }
+  ],
+  dessert: [
+    {
+      name: "Whisky Ice Cream",
+      price: "$100"
+    }
+  ]
+};
+
 class App extends Component {
   render() {
     return (
@@ -21,17 +42,47 @@ class App extends Component {
     );
   }
 }
+//menu: sections / sections are made of items ?
+//menu section
+//menu items
+const menuSection = ({ section }) => {
+  const poopOutItems = section.map(item => <MenuItem item />);
+
+  return (
+    <div className="container">
+      {poopOutItems}
+    </div>
+  );
+};
+const MenuItem = ({ menuItem }) => {
+  const { name, price } = menuItem;
+  return (
+    <div className="MenuItem well">
+      <p>
+        {name}
+      </p>
+      <p>
+        {price}
+      </p>
+    </div>
+  );
+};
+let makeSections = () => {
+  let sections = [];
+  for (let key in menu) {
+    sections.push(<MenuItem key />);
+  }
+  console.log("sections = ", sections);
+  return sections;
+};
 class Menu extends Component {
   render() {
     return (
       <div id="menu" className="menu">
-        <Nav />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>"this is the menu"</h2>
+        <h2>"this is the menu"</h2>
+        <div className="sections">
+          {makeSections()}
         </div>
-
-        <p className="App-intro">"menu stuff here"</p>
       </div>
     );
   }
@@ -40,7 +91,6 @@ class Directions extends Component {
   render() {
     return (
       <div id="directions" className="directions">
-        <Nav />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>"this is the menu"</h2>
@@ -55,7 +105,6 @@ class Pricing extends Component {
   render() {
     return (
       <div id="pricing" className="pricing">
-        <Nav />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>"this is the menu"</h2>
